adcd2879d58e387fc4c2e215621e109e5b0c7072
Chandrakant-Bhakare <161593114+Chandrakant-Bhakare@users.noreply.github.com>
2024-02-28 17:10:51 +0530

    Added Add and Demo file

---
+++ Add.java
@@ -0,0 +1,12 @@
+class Main Sum {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+    int third = 20;
+
+    int sum = first + second + third;
+    System.out.println(sum);
+  }
+}
